bbd70c80229d3316a5d11b0a1456af21fa75c70b
unknown <alexj.bartolo@gmail.com>
2023-01-26 12:09:47 +0100

    Added React Google Branch

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,13 +1,29 @@
 import React from "react"
 import { scroller } from "react-scroll"
+import { GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google'
+import axios from "axios"
+
 export default function Account () {
 
-    // function login () {
-    //     console.log("login")
-    // }
+    const [token, setToken] = React.useState("")
+    const [email, setEmail] = React.useState("")
+    const [calendar, setCalendar] = React.useState()
 
-    
+    React.useEffect(() => {
+        if(email) {
+            axios("https://www.googleapis.com/drive/v2/files",
+            {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+                .then(response => {
+                    console.log(response.data)
+                })
+        }
+    },[email])
+
     function change () {
+        
+
         scroller.scrollTo("drive", {
             smooth: true,
             offset: - document.getElementsByClassName("header")[0].offsetHeight - 30
@@ -19,7 +35,19 @@ export default function Account () {
             <span className="account--title">Google Account</span>
             <span className="account--email">alexj.bartolo@gmail.com</span>
             <button className="account--button" type="button" onClick={change}>Change Google Account</button>
-            {/* <button className="account--button account--login" type="button" onClick={login}>Login with Google</button> */}
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}><Button setEmail={setEmail} setToken={setToken}/></GoogleOAuthProvider>
+            
         </div>
     )
+}
+
+function Button (props) {
+
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => {console.log(tokenResponse);props.setToken(tokenResponse.access_token);props.setEmail("test")},
+        scope: "https://www.googleapis.com/auth/drive"
+    })
+
+
+    return <button className="account--button account--login" type="button" onClick={login}>Login with Google</button>
 }
